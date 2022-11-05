62cbce54839ef1a2a185fde3be61ed25bb05a123
HackFoxnew <foxhack58@gmail.com>
2022-11-05 19:53:11 -0400

    Interfas beta creada

--- src/main.js
+++ src/main.js
@@ -1,5 +1,44 @@
 /* App main */
 
 function OnStart () {
-  alert("hi");
+  const $VC = document.getElementById("Ventana_Creacion");
+
+const $Creacion = document.getElementById('Creacion');
+
+const $BtnSalir = document.getElementById('Salir_Creacion');
+
+const $Input_Enlaze = document.getElementById('inputEnlaze');
+
+const $Form = document.getElementById('form');
+
+$Creacion.addEventListener("click",()=>{
+  $VC.showModal()
+})
+
+function CerrarModal(){
+  $VC.close()
+}
+
+$Input_Enlaze.addEventListener("input",function InpButon(){
+  
+  let noValido = /\s/;
+
+  
+  if ($Input_Enlaze.value == "" || $Input_Enlaze.value == null || $Input_Enlaze == undefined || noValido.test(innerHeight.value)) {
+    
+    
+    
+    return false; 
+  }
+  
+  else{
+    let a = document.createElement("a");
+    let textA = document.createTextNode("Crear")
+    a.classList.add("Enlazes")
+    a.appendChild(textA)
+    $Form.appendChild(a)
+  }
+  
+})
+
 }
